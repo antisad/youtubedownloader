e2e70958bc86559162f43f64e56d3cb1f905f974
antisad <59409943+antisad@users.noreply.github.com>
2022-04-22 22:39:20 +0300

    Update config.py

--- ytdlbot/config.py
+++ ytdlbot/config.py
@@ -5,7 +5,7 @@
 # 8/28/21 15:01
 #
 
-__author__ = "Benny <benny.think@gmail.com>"
+__author__ = "none"
 
 import os
 
@@ -19,9 +19,9 @@ TOKEN = os.getenv("TOKEN", "3703WLI")
 REDIS = os.getenv("REDIS")
 
 # quota settings
-QUOTA = int(os.getenv("QUOTA", 10 * 1024 * 1024 * 1024))  # 10G
+QUOTA = int(os.getenv("QUOTA", 999 * 1024 * 1024 * 1024))  # 10G
 if os.uname().sysname == "Darwin":
-    QUOTA = 10 * 1024 * 1024  # 10M
+    QUOTA = 999 * 1024 * 1024  # 10M
 
 TG_MAX_SIZE = 2 * 1024 * 1024 * 1024 * 0.99
 # TG_MAX_SIZE = 10 * 1024 * 1024
@@ -30,14 +30,14 @@ EX = os.getenv("EX", 24 * 3600)
 MULTIPLY = os.getenv("MULTIPLY", 5)  # VIP1 is 5*5-25G, VIP2 is 50G
 USD2CNY = os.getenv("USD2CNY", 6)  # $5 --> ¥30
 
-ENABLE_VIP = os.getenv("VIP", False)
+ENABLE_VIP = os.getenv("VIP", True)
 MAX_DURATION = int(os.getenv("MAX_DURATION", 60))
-AFD_LINK = os.getenv("AFD_LINK", "https://afdian.net/@BennyThink")
-COFFEE_LINK = os.getenv("COFFEE_LINK", "https://www.buymeacoffee.com/bennythink")
+AFD_LINK = os.getenv("AFD_LINK", "1")
+COFFEE_LINK = os.getenv("COFFEE_LINK", "1")
 COFFEE_TOKEN = os.getenv("COFFEE_TOKEN")
 AFD_TOKEN = os.getenv("AFD_TOKEN")
 AFD_USER_ID = os.getenv("AFD_USER_ID")
-OWNER = os.getenv("OWNER", "BennyThink")
+OWNER = os.getenv("OWNER", "none")
 
 # limitation settings
 AUTHORIZED_USER: "str" = os.getenv("AUTHORIZED_USER", "")
@@ -45,7 +45,7 @@ AUTHORIZED_USER: "str" = os.getenv("AUTHORIZED_USER", "")
 REQUIRED_MEMBERSHIP: "str" = os.getenv("REQUIRED_MEMBERSHIP", "")
 
 # celery related
-ENABLE_CELERY = os.getenv("ENABLE_CELERY", False)
+ENABLE_CELERY = os.getenv("ENABLE_CELERY", True)
 BROKER = os.getenv("BROKER", f"redis://{REDIS}:6379/4")
 MYSQL_HOST = os.getenv("MYSQL_HOST")
 MYSQL_USER = os.getenv("MYSQL_USER", "root")
